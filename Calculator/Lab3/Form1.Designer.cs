2af7c739a4388dd34c587146c7ac4cfd26ac78be
kflank <91280677+kflank@users.noreply.github.com>
2022-07-24 15:50:18 -0700

    Add files via upload
    calculator desktop app using the .Net framework in C#

---
+++ Calculator/Lab3/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.Sub = new System.Windows.Forms.Button();
+            this.Mul = new System.Windows.Forms.Button();
+            this.Div = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(310, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Input: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Result:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(268, 231);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(268, 295);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(234, 26);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(517, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(295, 421);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(100, 40);
+            this.Add.TabIndex = 6;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Sub
+            // 
+            this.Sub.Location = new System.Drawing.Point(433, 421);
+            this.Sub.Name = "Sub";
+            this.Sub.Size = new System.Drawing.Size(100, 40);
+            this.Sub.TabIndex = 7;
+            this.Sub.Text = "Sub";
+            this.Sub.UseVisualStyleBackColor = true;
+            this.Sub.Click += new System.EventHandler(this.Sub_Click);
+            // 
+            // Mul
+            // 
+            this.Mul.Location = new System.Drawing.Point(295, 514);
+            this.Mul.Name = "Mul";
+            this.Mul.Size = new System.Drawing.Size(100, 40);
+            this.Mul.TabIndex = 8;
+            this.Mul.Text = "Mul";
+            this.Mul.UseVisualStyleBackColor = true;
+            this.Mul.Click += new System.EventHandler(this.Mul_Click);
+            // 
+            // Div
+            // 
+            this.Div.Location = new System.Drawing.Point(433, 514);
+            this.Div.Name = "Div";
+            this.Div.Size = new System.Drawing.Size(100, 40);
+            this.Div.TabIndex = 9;
+            this.Div.Text = "Div";
+            this.Div.UseVisualStyleBackColor = true;
+            this.Div.Click += new System.EventHandler(this.Div_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(947, 601);
+            this.Controls.Add(this.Div);
+            this.Controls.Add(this.Mul);
+            this.Controls.Add(this.Sub);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Sub;
+        private System.Windows.Forms.Button Mul;
+        private System.Windows.Forms.Button Div;
+    }
+}
+
---
+++ Calculator/Lab3/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// https://stackoverflow.com/questions/19747809/exception-handling-for-non-numeric-textbox-with-windows-form-in-c-sharp
+//I looked at this stack overflow page for figuring out try and catch blocks for my exception handling 
+
+namespace Lab3
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Kfir Flank - Lab3";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = string.Empty; //clear result textbox when clear is clicked
+            textBox2.Text = "0";
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {   
+            try
+            {
+                Double d = Double.Parse(textBox1.Text);
+                double a, b, sum;
+                a = Convert.ToDouble(textBox1.Text);
+                b = Convert.ToDouble(textBox2.Text);
+                sum = a + b;
+                textBox2.Text = Convert.ToString(sum);
+                textBox1.Text = string.Empty;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("invalid or missing value!");
+            }
+      
+        }
+
+        private void Sub_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Double d = Double.Parse(textBox1.Text);
+                double a, b, sum;
+                a = Convert.ToDouble(textBox1.Text);
+                b = Convert.ToDouble(textBox2.Text);
+                sum = b - a;
+                textBox2.Text = Convert.ToString(sum);
+                textBox1.Text = string.Empty;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("invalid or missing value!");
+            }
+        }
+
+        private void Mul_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Double d = Double.Parse(textBox1.Text);
+                double a, b, sum;
+                a = Convert.ToDouble(textBox1.Text);
+                b = Convert.ToDouble(textBox2.Text);
+                sum = a * b;
+                textBox2.Text = Convert.ToString(sum);
+                textBox1.Text = string.Empty;
+            }
+
+            catch (FormatException)
+            {
+                MessageBox.Show("invalid or missing value!");
+            }
+        }
+
+        private void Div_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Double d = Double.Parse(textBox1.Text);
+                double a, b, sum;
+                a = Convert.ToDouble(textBox1.Text);
+                b = Convert.ToDouble(textBox2.Text);
+                sum = b / a;
+                textBox2.Text = Convert.ToString(sum);
+                textBox1.Text = string.Empty;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("invalid or missing value!");
+            }
+
+        }
+    }
+}
